2397768fbd30e518929d29fbdc24e0ab11fe88e9
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-05-11 21:43:20 +0000

    infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__"
+    import sys
+    
+    sum = 0
+    for i in range(len(sys.agrv) - 1)
+        sum += int(sys.argv[i +1])
+    print("{}".format(sum))
