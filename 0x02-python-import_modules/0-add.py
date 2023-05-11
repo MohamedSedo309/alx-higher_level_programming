859b63ff05b966feb5d5d76cbfc746d8c37445f9
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-05-11 20:21:02 +0000

    hot fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-   """Print 1 + 2 = 3"""
-   from add_0 import add
-   a = 1
-   b = 2
-   print("{} + {} = {}".format(a, b, add(a, b)))
+    """Print 1 + 2 = 3"""
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
