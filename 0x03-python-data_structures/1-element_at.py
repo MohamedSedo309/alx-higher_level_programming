e95d8e91001f0477de3b7ac6327b1766d1667acd
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-05-14 18:08:27 +0000

    return element at index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    i = len(my_list)
+    if idx < 0:
+        return None
+    elif idx > (i - 1):
+        return None
+    else:
+        return my_list[idx]
