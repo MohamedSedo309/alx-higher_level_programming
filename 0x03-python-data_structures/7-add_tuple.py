72b2a626c0e778b78440759fc80af2478f394a35
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-05-15 16:46:39 +0000

    hot fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,4 +2,4 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     table_a += (0, 0)
     table_b += (0, 0)
-    return (table_a[0] + table_b[0], table_a[1] + table_a[1])
+    return (table_a[0] + table_b[0], table_a[1] + table_b[1])
