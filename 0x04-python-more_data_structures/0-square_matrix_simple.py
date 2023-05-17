a19f738726724596f0774cd1b2e0445710b91e42
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-05-17 18:54:57 +0300

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    if not matrix:
+        return None
     new_mat = [[0 for j in range(len(matrix[i]))] for i in range(len(matrix))]
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
