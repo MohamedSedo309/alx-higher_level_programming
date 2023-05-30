838b5a86ba71ab669891ec4002fb4b66dc22043d
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-05-30 20:28:26 +0300

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -47,7 +47,7 @@ class Square:
         Args:
             value: sets size to value
         """
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
