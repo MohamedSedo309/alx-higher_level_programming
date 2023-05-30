9328dd585a2d01f51714d6943bb111a566eeb6da
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-05-30 17:39:09 +0000

    Write a class Square that defines a square by: (based on 4-square.py)

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+'''
+Define square class
+with size att.
+safe init
+then calculate area
+'''
+
+
+class Square:
+    """
+    class Square definition
+
+    Args:
+        size (int): size of a side
+
+    Functions:
+        __init__(self, size)
+        size(self)
+        size(self, value)
+        area(self)
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes square object
+
+        Attributes:
+            size (int): default = 0
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """"
+        Getter
+
+        Return: size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter
+
+        Args:
+            value: sets size to value
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Calculates area of square
+        Returns:
+            area
+        """
+        return (self.__size)**2
+        
+    def my_print(self):
+        """
+        print # as square according to the side length
+        """
+        n = self.__size
+        for j in range(n):
+            for i in range(n):
+                print("#", end=" ")
+            print()
