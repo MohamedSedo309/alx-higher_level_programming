d27f5d0bf85e7f03fd4a67e241f10765300085ea
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-05-30 20:19:30 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,33 +1,43 @@
 #!/usr/bin/python3
-'''
+"""
 Define square class
 with size att.
 safe init
 then calculate area
-'''
+"""
 
 
 class Square:
     """
-    Sqaure class defination
+    class Square definition
+
     Args:
-        size (int): size of a side in square
+        size (int): size of a side
 
     Functions:
         __init__(self, size)
         area(self)
     """
+
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        """
+        Initializes square
+
+        Attributes:
+            __size (int): default = 0
+        """
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     def area(self):
         """
-        calculate the square area
+        Calculates area for square
+
         Returns:
             area
         """
-        return (self.__size) ** 2
+        return (self.__size)**2
