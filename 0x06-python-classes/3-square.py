74bdcdeabccc6d29eb18eefa31b76584ca5aff2a
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-05-30 20:13:49 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -8,7 +8,15 @@ then calculate area
 
 
 class Square:
-    """Sqaure clase with size safe from errors"""
+    """
+    Sqaure class defination
+    Args:
+        size (int): size of a side in square
+
+    Functions:
+        __init__(self, size)
+        area(self)
+    """
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
