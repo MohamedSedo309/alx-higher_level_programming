fd92c45389fdb948307dd390cf688bdb7e543f2b
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-06-07 22:01:26 +0000

    creater rectangle with setter for its width and height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Module 1-rectangle.py
+create a class represents rectangle
+with width and height
+"""
+
+
+class Rectangle:
+    """
+    Represent a rectangle
+    has a width and height
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        
+    @property
+    def width(self):
+        return self.__width
+        
+    @width.setter
+    def width(self, w):
+        if not isinstance(w, int):
+            raise TypeError("width must be an integer")
+        elif w < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = w
+    
+    @property
+    def height(self):
+        return self.__height
+        
+    @height.setter
+    def height(self, h):
+        if not isinstance(h, int):
+            raise TypeError("height must be an integer")
+        elif h < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = h
