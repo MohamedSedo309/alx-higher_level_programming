b041d1bd2bb246baec9fbc8aede798e71a0834d6
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-06-07 21:40:32 +0000

    Write an empty class Rectangle that defines a rectangle:

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module 0-rectangle.py
+create a class represents rectangle
+"""
+class Rectangle:
+    def __init__(self):
+        pass
+    def __repr__(self):
+        pass
