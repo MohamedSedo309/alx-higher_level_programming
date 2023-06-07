34ce270ce13e6df73b01579cb99748ce01f9ce0b
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-06-08 01:09:08 +0300

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,6 +15,9 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """
         the init function
+        Args:
+            width (int): The width
+            height (int): The height
         """
         self.width = width
         self.height = height
