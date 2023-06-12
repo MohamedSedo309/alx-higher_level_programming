462b0e81dca69d5867123956aa267c207a599a42
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-06-13 02:48:47 +0300

    Update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
-"""Write a class BaseGeometry
-Public instance method: def area(self): that raises an Exception
-with the message area() is not implemented"""
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """creating empty class"""
-    
+    """create class base geometry."""
+
     def area(self):
         """area exception function"""
         raise Exception("area() is not implemented")
