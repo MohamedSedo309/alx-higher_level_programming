b1301a8ffd00750588c0e3a9c983363ebb948d6d
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-06-12 23:20:20 +0000

    Write a function that returns True if the object is exactly an instance of the specified class ; otherwise False

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Defines a class MyList that inherits from list"""
+
+
+def is_same_class(obj, a_class):
+    """check for an instance from object"""
+    return type(obj) is a_class
