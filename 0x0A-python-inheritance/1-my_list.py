ac0dd86ec46760c41bca1526e39ba5aa5464f148
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-06-12 22:52:02 +0000

    Write a class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Defines a class MyList that inherits from list"""
+
+
+class MyList(list):
+    """creating class"""
+    def print_sorted(self):
+        """sort all list items in ascending order"""
+        print(sorted(self))
