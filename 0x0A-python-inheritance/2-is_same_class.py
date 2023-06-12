c63a9999b631288856ecf87f104a1cb391ea4c3f
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-06-13 02:24:34 +0300

    Update 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""Defines a class MyList that inherits from list"""
+"""Write a function that returns True if the object is exactly
+an instance of the specified class ; otherwise False"""
 
 
 def is_same_class(obj, a_class):
