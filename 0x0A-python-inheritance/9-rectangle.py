a51ba71cb8d8c2b681eae08437b3d7e7f7ca9226
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-06-13 03:34:29 +0300

    Update 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -6,7 +6,9 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """creating class and check the width and height value"""
+    
     def __init__(self, width, height):
+        """init function"""
         self.__width = width
         self.__height = height
         self.integer_validator("width", width)
