95f5e6195ecafd4d1a1baa56873c03e1d4dad3b0
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-06-13 18:26:11 +0000

    fixing mode

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,6 +6,6 @@ a text file (UTF8) and returns the number of characters added"""
 
 def write_file(filename="", text=""):
     """append text to a file"""
-    with open(filename, mode="a", encoding="utf-8") as file:
+    with open(filename, "a", encoding="utf-8") as file:
         chars_count = file.write(text)
         return chars_count
