56ab282d3686029c312fe8f5969d654fa7dbeba3
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-06-13 00:17:35 +0000

    adding int validator function to base geometry

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """create class base geometry."""
+
+    def area(self):
+        """area exception function"""
+        raise Exception("area() is not implemented")
+        
+    def integer_validator(self, name, value):
+        """int validator"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
