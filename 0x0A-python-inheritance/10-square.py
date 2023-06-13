277fb8f5a793040b73238b2bb36fc4e587bc631d
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-06-13 03:43:07 +0300

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,14 +9,6 @@ class Square(Rectangle):
     
     def __init__(self, size):
         """override init method"""
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-        self.integer_validator("size", size)
-
-    def __str__(self):
-        """adding own description in str"""
-        return "[Square] {}/{}".format(self.__size, self.__size)
-
-    def area(self):
-        """Return the area of the Square"""
-        return self.__size ** 2
