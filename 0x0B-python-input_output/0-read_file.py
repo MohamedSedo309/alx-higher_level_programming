d738eae0dfdf927688df8f123e6f6a71d0c61e92
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-06-13 18:17:00 +0000

    hot fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(filename=""):
     """open the file then print its content"""
     with open(filename, encoding="utf-8") as file:
         text = file.read()
-        print(text)
+        print(text, end="")
