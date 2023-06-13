c07102e25118d056b56108a3c398c018d2d90c3e
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-06-13 00:39:58 +0000

    Write a class Square that inherits from Rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Defines a Square class inherit from Rectangle."""
+BaseGeometry = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    """Create a square class"""
+    
+    def __init__(self, size):
+        """override init method"""
+        super().__init__(size, size)
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def __str__(self):
+        """adding own description in str"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
+    def area(self):
+        """Return the area of the Square"""
+        return self.__size ** 2
