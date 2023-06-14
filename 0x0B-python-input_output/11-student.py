d428cfe2dc7440586194b76e1deaed67d0c6bb01
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-06-14 23:51:34 +0000

    Write a class Student that defines a student by: (based on 10-student.py)

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""create a class Student."""
+
+
+class Student:
+    """class student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Get Student to json """
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        extract json student
+        """
+        for i, j in json.items():
+            setattr(self, i, j)
