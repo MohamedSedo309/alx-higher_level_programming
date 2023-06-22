35ebd5152100ecfa56a9f5593ba4afccdbe038b8
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-06-22 17:49:16 +0000

    Update the class Rectangle by updating the public method def update(self, *args): by changing the prototype to update(self, *args, **kwargs) that assigns a key/value argument to attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,14 +91,12 @@ class Rectangle(Base):
         
         return my_rec + my_id + my_xy + my_wh
 
-    def update(self, *args):
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+    def update(self, *args, **kwargs):
+        """create update method"""
+        if args is not None and len(args) is not 0:
+            att_list = ['id', 'width', 'height', 'x', 'y']
+            for i in range(len(args)):
+                setattr(self, att_list[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
