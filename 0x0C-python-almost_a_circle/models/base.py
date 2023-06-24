4bd6370775cbe905c9ea55552f8de9b46780a61f
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-06-24 00:54:37 +0000

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,8 @@
 #usr/bin/python3
 """models/base.py Module"""
+import json
+import os.path
+import csv
 
 
 class Base:
@@ -13,3 +16,27 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ convert list to json string """
+        if list_dictionaries is None or list_dictionaries == "[]":
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Save object in a file """
+        filename = "{}.json".format(cls.__name__)
+        mymap = []
+
+        if not list_objs:
+            pass
+        else:
+            for i in range(len(list_objs)):
+                mymap.append(list_objs[i].to_dictionary())
+
+        lists = cls.to_json_string(mymap)
+
+        with open(filename, 'w') as f:
+            f.write(lists)
