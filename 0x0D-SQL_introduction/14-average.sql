42ac539db9c58f58e4babae1b8a85d55a1217d0b
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-07-20 13:07:46 -0700

    hot fix

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,2 @@
---Write a script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
---The result column name should be average
+-- Write a script that computes the score average of all records in the table second_table of the database hbtn_0c_0 The result column name should be average
 SELECT AVG(score) AS average FROM second_table;
