9547be01eb5489b6dd5412f8b9b707bd5db890d5
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-08-15 01:02:35 +0300

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
 const arg = process.argv[2];
 num = parseInt(arg);
-if (isNaN(num)) {
-    console.log('Missing number of occurrences')
-} else {
-    for (i = 0; i < num; i++) {
-        console.log('C is fun')
-    }
-}
+if (isNaN(num)) { console.log('Missing number of occurrences') } else { for (i = 0; i < num; i++) { console.log('C is fun') } }
