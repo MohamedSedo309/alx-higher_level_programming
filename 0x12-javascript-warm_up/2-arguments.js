457facc40e6ee8fcd3e78aa11bdfef472b45a69e
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-08-14 20:57:29 +0300

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-let args = process.argv.slice(2);
+const args = process.argv.slice(2);
 if (args.length === 0) { console.log('No argument'); } else if (args.length === 1) { console.log('Argument found'); } else { console.log('Arguments found'); }
