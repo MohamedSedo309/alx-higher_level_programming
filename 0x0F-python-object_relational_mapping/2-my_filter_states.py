61ee8b218c12fa7e2587f12dfdfb9ae8a14103ac
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-08-20 04:50:40 +0300

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
     cursor.execute("SELECT COUNT(*) FROM states WHERE name = %s", (argv[4],))
     count = cursor.fetchone()[0]
-    cursor.execute("""SELECT * FROM states WHERE name = %s ORDER BY id ASC""",(argv[4],))
+    cursor.execute("""SELECT * FROM states WHERE name = '{s}' ORDER BY id ASC""".format(argv[4]))
     for row in cursor.fetchall():
         if row[1] == argv[4]:
             print(row)
