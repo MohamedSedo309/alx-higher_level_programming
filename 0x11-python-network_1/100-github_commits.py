5a3987fdd4fd89f38f8517c5b076bf0643702238
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-09-02 01:49:00 +0000

    get 10 last commits from git hub repo

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+interview task !
+Please list 10 commits (from the most recent to oldest)
+of the  by the 
+You must use the GitHub API, here is the documentation
+https://developer.github.com/v3/repos/commits/
+Print all commits by: `<sha>: <author name>` (one by line)
+"""
+
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = "https://developer.github.com/v3/repos/{}/{}/commits/".format(argv[2], argv[1])
+    res = requests.get(url)
+    commits = res.json()
+    for commit in commits[0:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))railsuser railsrepository 
