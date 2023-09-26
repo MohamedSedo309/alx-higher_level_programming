7e82224644e9a46ca43de21831ae0def9e9cba38
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-09-26 19:51:20 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/7-multi_c.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-const arg = process.argv[2];
-num = parseInt(arg);
-if (isNaN(num)) { console.log('Missing number of occurrences') } else { for (i = 0; i < num; i++) { console.log('C is fun') } }
