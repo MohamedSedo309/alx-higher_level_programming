c14b1880c210ce173170ed6974b4d5abc7e665f2
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2024-01-09 18:56:08 +0200

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const y = process.argv[2];
 const x = parseInt(y);
-if (isNaN(x)) { console.log('Not a number'); } else { console.log(x); }
+if (isNaN(x)) { console.log('Not a number'); } else { console.log(`My number: ${x}`); }
