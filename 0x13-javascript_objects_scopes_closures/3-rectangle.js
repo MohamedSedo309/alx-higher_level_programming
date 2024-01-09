e77ca86fb4d548ed05f961a8f3140d861adb7a6c
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2024-01-09 21:06:22 +0200

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor (w, h) {
+  constructor(w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
@@ -9,7 +9,7 @@ module.exports = class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-	console.log('X'.repeat(this.width));
+      console.log('X'.repeat(this.width));
     }
   }
 };
