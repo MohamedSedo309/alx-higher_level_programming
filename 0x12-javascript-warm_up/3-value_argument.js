4cbdaa8b5213d0aa51674ae9d1df6e2c4c4a3b1c
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2024-01-09 17:42:36 +0200

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const argu = process.argv[2];
-if (argu === null) { console.log('No argument'); } else { console.log(argu); }
+if (argu === undefined) { console.log('No argument'); } else { console.log(argu); }
