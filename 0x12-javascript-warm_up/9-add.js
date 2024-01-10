cd7e57345d541ecba7e4c4ef3589d71f015d2a3a
MohamedSedo309 <mohamedsedo1907@gmail.com>
2024-01-10 14:26:10 -0800

    task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,10 +3,10 @@ const args = process.argv.slice(2);
 const x = parseInt(args[0]);
 const y = parseInt(args[1]);
 if (isNaN(x) || isNaN(y)) {
-    console.log('NaN');
+  console.log('NaN');
 } else {
-    console.log(add(x, y));
+  console.log(add(x, y));
 }
-function add(a, b) {
-    return a + b;
+function add (a, b) {
+  return a + b;
 }
