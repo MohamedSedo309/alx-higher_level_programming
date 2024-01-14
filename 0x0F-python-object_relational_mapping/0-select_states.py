ea62fc794a0f3495ff5fd699dd4697992101e028
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2024-01-14 02:48:13 +0200

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,11 @@ import MySQLdb
 if __name__ == "__main__":
     
     #connect to db
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
     
     # create cursor to exec queries using SQL
     cursor = db.cursor()
