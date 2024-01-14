a1578d2be0e9934c1236508b9da5046b61a1506b
MohamedSedo309 <mohamedelabd1907@gmail.com>
2024-01-13 17:11:13 -0800

    trying to find the fucking error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                          user=argv[1],
                          passwd=argv[2],
                          db=argv[3]
-                        )
+                         )
     # create a cursor to execute queries using SQL
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
