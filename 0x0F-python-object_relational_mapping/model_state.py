7b5452ebfe6fd104af14c7b4927e99766b785b6f
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2024-01-20 03:39:29 +0200

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,6 +8,3 @@ class State(Base):
     
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-engine = create_engine('mysql://username:password@localhost:3306/hbtn_0e_6_usa')
-Base.metadata.create_all(engine)
