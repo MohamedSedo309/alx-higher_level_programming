a3575cb505636866fba993a3ff766f4851295cbf
MohamedSedo309 <mohamedelabd1907@gmail.com>
2024-01-19 18:27:35 -0800

    fix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -13,8 +13,9 @@ if __name__ == "__main__":
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
-    
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(user, passwd, db), pool_pre_ping=True)
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(user, passwd, db), pool_pre_ping=True)
     Session = (sessionmaker(bind=engine))
     session = Session()
     state = session.query(State).filter_by(name=argv[4]).first()
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,11 +13,13 @@ if __name__ == "__main__":
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
-    
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(user, passwd, db), pool_pre_ping=True)
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(user, passwd, db), pool_pre_ping=True)
     Session = (sessionmaker(bind=engine))
     session = Session()
-    filtered_res = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
+    filtered_res = session.query(State).filter(
+        State.name.like('%a%')).order_by(State.id)
     for state in filtered_res:
         print("{:d}: {:s}".format(state.id, state.name))
     session.close()
