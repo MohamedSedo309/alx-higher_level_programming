a2a863dd84b2b1002a69575b5633f6dd51f4a98d
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2024-01-20 04:09:32 +0200

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -15,5 +15,6 @@ class State(Base):
     acting as MySQL table "states"
     """
     __tablename__ = "states"
+    
     id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
