3fd0e1e65053445bb561a620834daf2f324961d7
MohamedSedo309 <mohamedsedo1907@gmail.com>
2024-01-26 11:54:50 -0800

    post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+adding some attr to the response
+sending post request to url , with email data
+"""
+
+
+from sys import argv
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    url = argv[1]
+    values = {'email' : argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
