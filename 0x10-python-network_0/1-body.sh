be057f20aacad84f3c1492166ec28dd02baee742
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2024-02-01 19:57:41 +0200

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
-
-# Check if a URL is provided as an argument
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-url="$1"
-
-# Send a GET request to the URL and store the response in a variable
-response=$(curl -s -w "%{http_code}" "$url")
-
-# Extract the status code
-status_code=$(echo "$response" | tail -n1)
-
-# Check if the status code is 200
-if [ "$status_code" -eq 200 ]; then
-  # Extract and display the body of the response
-  body=$(echo "$response" | sed '$d')
-  echo "$body"
-else
-  echo "Request failed with status code: $status_code"
-fi
+# get respose body
+curl -s -w "%{http_code}" "$url" | awk 'END { if ($0 == 200) print $0 }'
