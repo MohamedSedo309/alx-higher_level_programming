3246b430e11e5ea34b18300144a7c51029d9c8b1
MohamedSedo309 <mohamedelabd1907@gmail.com>
2024-02-01 09:54:33 -0800

    hot fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
-# Take in URL, display body of a 200 response; Usage: ./1-body.sh 5B0.0.0.0:5000/route_1 ; echo ""
-curl -sL "$1
+
+# Check if a URL is provided as an argument
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+url="$1"
+
+# Send a GET request to the URL and store the response in a variable
+response=$(curl -s -w "%{http_code}" "$url")
+
+# Extract the status code
+status_code=$(echo "$response" | tail -n1)
+
+# Check if the status code is 200
+if [ "$status_code" -eq 200 ]; then
+  # Extract and display the body of the response
+  body=$(echo "$response" | sed '$d')
+  echo "$body"
+else
+  echo "Request failed with status code: $status_code"
+fi
